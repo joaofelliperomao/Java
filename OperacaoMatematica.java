c2e5b25fa975c2796b38d26dabcbcf3f25547e3a
João Fellipe Romão Feitosa <163138415+joaofelliperomao@users.noreply.github.com>
2024-09-19 11:14:03 -0300

    Add files via upload

---
+++ OperacaoMatematica.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.controle;
+
+public class OperacaoMatematica {
+    public Double calcular(Double valor1, Double valor2) {
+    return 0.0;
+    }
+    
+}
---
+++ Soma.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+public class Soma extends OperacaoMatematica{
+    public Double calcular(Double valor1,Double valor2){
+        return valor1+valor2;
+    }
+    
+}
