b1e4c778fe06f7f6aa073427a7198d696905bb01
João Fellipe <163138415+joaofelliperomao@users.noreply.github.com>
2024-11-14 11:16:39 -0300

    Add files via upload

---
+++ controle/Aluno.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author Joao.romao
+ */
+public class Aluno {
+
+    /**
+     * @param args the command line arguments
+     */
+
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+    private String nome;
+    private String email;
+    private int codigo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+
+    }
